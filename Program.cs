71f7f391dcb95afe7c0589645ac4afb074db19ea
Christos Matskas <chmatsk@microsoft.com>
2021-05-28 16:39:00 -0700

    Added AAD Authentication

---
+++ Program.cs
@@ -0,0 +1,22 @@
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddCors(options => options.AddPolicy("allowAny", o => o.AllowAnyOrigin()));
+builder.Services.AddMicrosoftIdentityWebApiAuthentication(builder.Configuration);
+builder.Services.AddAuthorization();
+
+var app = builder.Build();
+
+app.UseCors();
+app.UseAuthentication();
+app.UseAuthorization();
+
+app.MapGet("/secure", [EnableCors("allowAny")] () =>
+{
+    return "hello from secure";
+}).RequireAuthorization();
+
+app.MapGet("/insecure", [EnableCors("allowAny")] () =>
+{
+    return "hello from insecure";
+});
+
+app.Run();
